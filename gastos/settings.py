f0a374aa2e8f7e74591ad34534e080179e53b832
weverthondev <weverthondev@gmail.com>
2026-03-05 14:10:25 -0300

    fix: corrige configuração da static files

--- gastos/settings.py
+++ gastos/settings.py
@@ -69,4 +69,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
