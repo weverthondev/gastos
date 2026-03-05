5cd6d76d9ea286a7991f06b639c58a15f6316c68
weverthondev <weverthondev@gmail.com>
2026-03-05 14:33:40 -0300

    fix: configura PostgreSQL

--- gastos/settings.py
+++ gastos/settings.py
@@ -1,5 +1,6 @@
 from decouple import config
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,10 +49,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'gastos.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
